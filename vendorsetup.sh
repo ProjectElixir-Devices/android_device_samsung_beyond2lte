98599eb6ad8b6800b71a575adfaa0f0a460140d5
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-10-09 13:10:52 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,12 @@ echo 'Starting Cloning repos for beyond2lte'
 echo 'Cloning Kernel tree [1/7]'
 # Kernel for exynos9820
 rm -rf kernel/samsung/exynos9820
-git clone https://github.com/CherishOS-exynos9820/android_kernel_samsung_exynos9820 kernel/samsung/exynos9820 -b android-13-upstream
+git clone https://github.com/linux4-bringup-priv/android_kernel_samsung_exynos9820 -b lineage-21.0 kernel/samsung/exynos9820
 
 echo 'Cloning Common Tree [2/7]'
 # Common tree for exynos9820-common
 rm -rf device/samsung/exynos9820-common
-git clone https://github.com/ProjectElixir-Devices/android_device_samsung_exynos9820-common device/samsung/exynos9820-common
+git clone https://github.com/ProjectElixir-Devices/android_device_samsung_exynos9820-common -b ELIXIR-14 device/samsung/exynos9820-common
 
 echo 'Cloning Device Tree [3/7]'
 # Device tree for beyond2lte
@@ -19,9 +19,9 @@ git clone https://github.com/ProjectElixir-Devices/android_device_samsung_beyond
 echo 'Cloning Vendor Trees [4/7]'
 # Vendor blobs for beyond2lte
 rm -rf vendor/samsung/beyond2lte
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_beyond2lte vendor/samsung/beyond2lte
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_beyond2lte -b lineage-21.0 vendor/samsung/beyond2lte
 rm -rf vendor/samsung/exynos9820-common
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_exynos9820-common vendor/samsung/exynos9820-common
+git clone https://github.com/linux4-bringup-priv/android_vendor_samsung_exynos9820-common -b lineage-21.0 vendor/samsung/exynos9820-common
 
 echo 'Cloning Hardware Samsung [5/7]'
 # Hardware OSS parts for Samsung
@@ -33,14 +33,14 @@ mv nfc hardware/samsung
 echo 'Cloning Samsung_Slsi and Linaro BSP repos [6/7]'
 # SLSI Sepolicy
 rm -rf device/samsung_slsi/sepolicy
-git clone https://github.com/FreeRunner4ever/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
+git clone https://github.com/linux4-bringup-priv/android_device_samsung_slsi_sepolicy -b lineage-21.0 device/samsung_slsi/sepolicy
 # Linaro BSP
 rm -rf hardware/samsung_slsi-linaro
-git clone https://github.com/FreeRunner4ever/android_hardware_samsung_slsi-linaro_graphics.git hardware/samsung_slsi-linaro/graphics
-git clone https://github.com/FreeRunner4ever/android_hardware_samsung_slsi-linaro_config.git hardware/samsung_slsi-linaro/config
-git clone https://github.com/FreeRunner4ever/android_hardware_samsung_slsi-linaro_exynos.git hardware/samsung_slsi-linaro/exynos
-git clone https://github.com/FreeRunner4ever/android_hardware_samsung_slsi-linaro_openmax.git hardware/samsung_slsi-linaro/openmax
-git clone https://github.com/FreeRunner4ever/android_hardware_samsung_slsi-linaro_exynos5.git hardware/samsung_slsi-linaro/exynos5
+git clone https://github.com/linux4-bringup-priv/android_hardware_samsung_slsi-linaro_graphics.git -b lineage-21.0 hardware/samsung_slsi-linaro/graphics
+git clone https://github.com/linux4-bringup-priv/android_hardware_samsung_slsi-linaro_config.git -b lineage-21.0 hardware/samsung_slsi-linaro/config
+git clone https://github.com/linux4-bringup-priv/android_hardware_samsung_slsi-linaro_exynos.git -b lineage-21.0 hardware/samsung_slsi-linaro/exynos
+git clone https://github.com/linux4-bringup-priv/android_hardware_samsung_slsi-linaro_openmax.git -b lineage-21.0 hardware/samsung_slsi-linaro/openmax
+git clone https://github.com/linux4-bringup-priv/android_hardware_samsung_slsi-linaro_exynos5.git -b lineage-21.0 hardware/samsung_slsi-linaro/exynos5
 
 echo 'Cloning Sepolicy Dependency repo [7/7]'
 # Sepolicy
